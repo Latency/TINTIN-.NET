0da03df206ab8500705995d9c235658b9c62be96
Latency McLaughlin <latency@bio-hazard.us>
2017-09-17 12:59:40 -0700

    Updated help documentation.

---
+++ Source/Enums/Events.cs
@@ -0,0 +1,45 @@
+﻿//  *****************************************************************************
+//  File:       Events.cs
+//  Solution:   TinTin.NET
+//  Project:    TinTin
+//  Date:       09/17/2017
+//  Author:     Latency McLaughlin
+//  Copywrite:  Bio-Hazard Industries - 1998-2017
+//  *****************************************************************************
+
+// ReSharper disable InconsistentNaming
+namespace TinTin.Enums {
+  public enum Events {
+    DATE,
+    DAY,
+    END_OF_PATH,
+    HOUR,
+    IAC,
+    IAC_SB_MSDP,
+    IAC_SB_MSSP,
+    IAC_SB_NEW_ENVIRON,
+    IAC_SB_ZMP,
+    IAC_SB,
+    MAP_ENTER_MAP,
+    MAP_ENTER_ROOM,
+    MAP_EXIT_MAP,
+    MAP_EXIT_ROOM,
+    MINUTE,
+    MONTH,
+    PROGRAM_START,
+    PROGRAM_TERMINATION,
+    RECEIVED_INPUT,
+    RECEIVED_LINE,
+    RECEIVED_OUTPUT,
+    SCREEN_RESIZE,
+    SECOND,
+    SEND_OUTPUT,
+    SESSION_ACTIVATED,
+    SESSION_CONNECTED,
+    SESSION_DEACTIVATED,
+    SESSION_DISCONNECTED,
+    TIME,
+    WEEK,
+    YEAR
+  }
+}
